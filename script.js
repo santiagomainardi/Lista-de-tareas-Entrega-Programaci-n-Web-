b42d382ca218f988d6d8b7f4a4f03dfa558dde4e
santiagomainardi <smainardi@itba.edu.ar>
2022-11-18 00:40:01 -0300

    a

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 // Funciones
 // Info date
 const dateNumber = document.getElementById('dateNumber');
+const dateText = document.getElementById('dateText');
+const dateMonth = document.getElementById('dateMonth');
+const dateYear = document.getElementById('dateYear');
 
 // Tasks Container
-const tasksContainer = document.getElementById('contenedorTareas');
+const contenedorTareas = document.getElementById('contenedorTareas');
 
 const setDate = () => {
     const date = new Date();
     dateNumber.textContent = date.toLocaleString('es', { day: 'numeric' });
+    dateText.textContent = date.toLocaleString('es', { weekday: 'long' });
+    dateMonth.textContent = date.toLocaleString('es', { month: 'short' });
+    dateYear.textContent = date.toLocaleString('es', { year: 'numeric' });
 };
 
 const addNewTask = event => {
-    event.preventDefault();
-    const { value } = event.target.taskText;
+    event.preventDefault(); 
+    const { value } = event.target.tareaTexto;
     if(!value) return;
-    const task = document.createElement('div');
-    task.classList.add('task', 'roundBorder');
-    task.addEventListener('click', changeTaskState)
-    task.textContent = value;
-    tasksContainer.prepend(task);
+    const tarea = document.createElement('div');
+    tarea.classList.add('tarea');
+    tarea.addEventListener('click', changeTaskState)
+    tarea.textContent = value;
+    contenedorTareas.prepend(tarea);
     event.target.reset();
 };
 
@@ -26,21 +32,7 @@ const changeTaskState = event => {
     event.target.classList.toggle('done');
 };
 
-const order = () => {
-    const done = [];
-    const toDo = [];
-    tasksContainer.childNodes.forEach( el => {
-        el.classList.contains('done') ? done.push(el) : toDo.push(el)
-    })
-    return [...toDo, ...done];
-}
-
-const renderOrderedTasks = () => {
-    order().forEach(el => tasksContainer.appendChild(el))
-}
-
 setDate();
-
 
 /* 
 LISTA DE TAREAS
